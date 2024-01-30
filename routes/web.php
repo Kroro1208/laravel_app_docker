0705810d6c9c17fc55d6cb84a71351b74402e619
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-01-30 16:23:32 +0900

    RESTfulなルートを設定(auth付)、簡単なindexメソッドを実装

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\ContactFormController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get("tests/test", [TestController::class, "index"]);
+Route::middleware(['auth'])->resource('contacts', ContactFormController::class);
 
 Route::get('/', function () {
     return view('welcome');
@@ -28,4 +33,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
