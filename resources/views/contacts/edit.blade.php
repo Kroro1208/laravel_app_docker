3800727d4ca08d423ad7c8097cac05904786427d
kuroro1208 <yamagata_7580@yahoo.co.jp>
2024-01-31 12:46:03 +0900

    updateメソッド実装, edit.blade.phpのformタグ修正

--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -10,8 +10,9 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <section class="text-gray-600 body-font relative">
-                        <form method="post" action="{{ route('contacts.store') }}">
+                        <form method="post" action="{{ route('contacts.update', ['contact' => $contact->id])}}">
                             @csrf
+                            @method('PUT')
                             <div class="container px-5 py-18 mx-auto">
                                 <div class="lg:w-1/2 md:w-2/3 mx-auto">
                                     <div class="flex flex-wrap -m-2">
@@ -71,9 +72,8 @@
                                                 <textarea id="contact" name="contact" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out">{{ $contact->contact }}</textarea>
                                             </div>
                                         </div>
-
                                         <div class="p-2 w-full">
-                                            <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">送信する</button>
+                                            <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">更新する</button>
                                         </div>
                                     </div>
                                 </div>
